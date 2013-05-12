ee246bd42fe7c65c4e07c9b7765823a32d18b9c3
Xwybylty Soslan <soslanx@gmail.com>
2013-05-12 10:49:03 +0400

    clean-up

--- ars.ino
+++ ars.ino
@@ -1,21 +1,6 @@
 int pin, val, nextFlag=0, record=0, savedPin=0, flag=0;
 float a,t,v,v2;
-int incomingByte = 0; 
-#define AREF_VOLTAGE 3.3
-
-/*long readVcc() { 
-long result; 
-ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-delay(2); // Wait for Vref to settle 
-ADCSRA |= _BV(ADSC); // Convert 
-while (bit_is_set(ADCSRA,ADSC)); 
-result = ADCL; 
-result |= ADCH<<8; 
-result = 1125300L / result; 
-// Back-calculate AVcc in mV 
-return result/1000; 
-}*/
-
+int incomingByte = 0;
 
 void setup(){
   Serial.begin(9600);
@@ -100,7 +85,5 @@ void loop(){
     }
    }
   delay(100);
-  
-  
 }
 
