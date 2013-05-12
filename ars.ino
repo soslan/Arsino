b43aa3dc4bc8eb76e7570c369a4b363d54b5e822
Xwybylty Soslan <soslanx@gmail.com>
2013-05-12 10:16:46 +0400

    initial version of ars.ino

---
+++ ars.ino
@@ -0,0 +1,106 @@
+int pin, val, nextFlag=0, record=0, savedPin=0, flag=0;
+float a,t,v,v2;
+int incomingByte = 0; 
+#define AREF_VOLTAGE 3.3
+
+/*long readVcc() { 
+long result; 
+ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+delay(2); // Wait for Vref to settle 
+ADCSRA |= _BV(ADSC); // Convert 
+while (bit_is_set(ADCSRA,ADSC)); 
+result = ADCL; 
+result |= ADCH<<8; 
+result = 1125300L / result; 
+// Back-calculate AVcc in mV 
+return result/1000; 
+}*/
+
+
+void setup(){
+  Serial.begin(9600);
+  pin=0;
+  analogReference(INTERNAL);
+  
+}
+
+float getVoltage(int pin){
+  float a, v;
+  a=analogRead(pin);
+  v=1.1 * a * 1000/1024;
+  return v;
+}
+
+boolean processDigit(byte b){
+  if(b>='0' && b<='9'){
+    record=record*10+(b-'0');
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+int readNumber(){
+  int record=0;
+  byte b;
+  while(Serial.available()>0){
+    b=Serial.read();
+    if(b>='0' && b<='9'){
+      record=record*10+(b-'0');
+    }
+    else{
+      return record;
+    }
+    
+  }
+}
+
+void loop(){
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    switch(flag){
+      case 0:
+        switch(incomingByte){
+          case 'a':
+            pin=readNumber();
+            Serial.println(getVoltage(pin));
+            break;
+          case 'd':
+            pin=readNumber();
+            Serial.println(digitalRead(pin));
+            break;
+          case 'i':
+            pin=readNumber();
+            pinMode(pin, INPUT);
+            break;
+          case 'o':
+            pin=readNumber();
+            pinMode(pin, OUTPUT);
+            flag=0;
+            break;
+          case 'D':
+            pin=readNumber();
+            val=readNumber();
+            if(val==0){
+              digitalWrite(pin, LOW);
+            }
+            else{
+              digitalWrite(pin, HIGH);
+            }
+            break;
+          case 'A':
+            pin=readNumber();
+            val=readNumber();
+            analogWrite(pin, val);
+            break;
+            
+        }
+        break;
+    }
+   }
+  delay(100);
+  
+  
+}
+
